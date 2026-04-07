709258975b5162edfd8a28a84a52ddccb889b32e
quangduy <126586796+QuangDuy212@users.noreply.github.com>
2026-04-07 18:05:59 +0700

    feat: first-one

--- backend/src/main/java/com/study4you/config/SecurityConfig.java
+++ backend/src/main/java/com/study4you/config/SecurityConfig.java
@@ -62,7 +62,14 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173", "http://localhost:8081"));
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost", 
+                "http://127.0.0.1", 
+                "http://localhost:80", 
+                "http://localhost:3000", 
+                "http://localhost:5173", 
+                "http://localhost:8081"
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
